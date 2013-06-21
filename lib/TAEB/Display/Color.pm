cb9318fc30425820e847d4fb4a71ce72f20d4eab
Shawn M Moore <code@sartak.org>
2013-06-21 18:51:56 -0400

    Get rid of debug Color overloading

--- lib/TAEB/Display/Color.pm
+++ lib/TAEB/Display/Color.pm
@@ -25,19 +25,6 @@ sub standard_index {
     return $self->index + 8 * $self->bold;
 }
 
-# XXX this is just a temporary debugging measure while I convert the code to sane color usage :)
-use Scalar::Util 'refaddr';
-use overload (
-    fallback => 1,
-    q{==} => sub {
-        my ($a, $b) = @_;
-        return 0 if !defined($a) || !defined($b);
-
-        confess unless ref($a) && ref($b) && $a->isa(__PACKAGE__) && $b->isa(__PACKAGE__);
-        return refaddr($a) == refaddr($b);
-    },
-);
-
 __PACKAGE__->meta->make_immutable;
 
 1;
