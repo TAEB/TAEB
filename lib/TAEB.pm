eb93d6061e594a1d7d55e605e5c666699a8be1a7
Shawn M Moore <code@sartak.org>
2013-06-22 16:20:01 -0400

    Revert "Delegate inventory and inventory_items"
    This reverts commit 1fd38d9d010ca0c8dc7c226effa227840f9992e5.

--- lib/TAEB.pm
+++ lib/TAEB.pm
@@ -292,10 +292,8 @@ class_has item_pool => (
     isa     => 'TAEB::World::ItemPool',
     default => sub { TAEB::World::ItemPool->new },
     handles => {
-        get_artifact    => 'get_artifact',
-        seen_artifact   => 'get_artifact',
-        inventory       => 'inventory',
-        inventory_items => 'inventory_items',
+        get_artifact  => 'get_artifact',
+        seen_artifact => 'get_artifact',
     },
 );
 
@@ -598,6 +596,10 @@ sub new_item {
     $taeb_class->meta->rebless_instance($item);
     return $item;
 }
+
+sub inventory { shift->item_pool->inventory }
+
+sub inventory_items { shift->item_pool->inventory->items }
 
 sub has_item {
     my $self = shift;
