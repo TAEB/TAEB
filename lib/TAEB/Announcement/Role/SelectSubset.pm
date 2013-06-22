8801f7eb260052386585ba8a62e70f72ab6b08ef
Shawn M Moore <code@sartak.org>
2013-06-22 12:13:13 -0400

    Clean up SelectSubset for auto_deref. refs TAEB/TAEB #35

--- lib/TAEB/Announcement/Role/SelectSubset.pm
+++ lib/TAEB/Announcement/Role/SelectSubset.pm
@@ -2,36 +2,43 @@ package TAEB::Announcement::Role::SelectSubset;
 use Moose::Role;
 
 has items => (
-    is         => 'ro',
+    traits     => ['Array'],
     isa        => 'ArrayRef',
     required   => 1,
-    auto_deref => 1,
+    handles    => {
+        items      => 'elements',
+        item_count => 'count',
+        item       => 'get',
+    },
 );
 
 has _is_selected => (
     is      => 'ro',
     isa     => 'ArrayRef',
     lazy    => 1,
-    default => sub { [ 0 x shift->items ] },
+    default => sub { [ 0 x shift->item_count ] },
 );
 
 sub select { ## no critic (ProhibitBuiltinHomonyms)
     my $self = shift;
 
-    for my $selection (@_) {
-        for my $index (0 .. $self->items - 1) {
-            my $item = $self->items->[$index];
-            $self->_is_selected->[$index] = 1 if $item eq $selection;
+    SELECTION: for my $selection (@_) {
+        for my $index (0 .. $self->item_count - 1) {
+            my $item = $self->item($index);
+            if ($item eq $selection) {
+                $self->_is_selected->[$index] = 1;
+                next SELECTION;
+            }
         }
     }
 }
 
-sub selected {
+sub selected_items {
     my $self = shift;
 
-    return map  { $self->items->[$_] }
+    return map  { $self->item($_) }
            grep { $self->_selected->[$_] }
-           0 .. $self->items - 1;
+           0 .. $self->item_count - 1;
 }
 
 no Moose::Role;
