5b2f407975b34e44ab49c18d676c044714ec5721
Shawn M Moore <code@sartak.org>
2013-06-30 19:26:23 -0400

    IdentifyItems is also an ItemMenu that pulls from inventory

--- lib/TAEB/Announcement/Query/IdentifyItems.pm
+++ lib/TAEB/Announcement/Query/IdentifyItems.pm
@@ -2,7 +2,12 @@ package TAEB::Announcement::Query::IdentifyItems;
 use Moose;
 use TAEB::OO;
 extends 'TAEB::Announcement::Query';
-with 'TAEB::Announcement::Role::SelectSubset';
+with (
+    'TAEB::Announcement::Role::SelectSubset',
+    'TAEB::Announcement::Role::ItemMenu',
+);
+
+sub items_from { TAEB->inventory_items }
 
 __PACKAGE__->meta->make_immutable;
 
