2a0e1723a8567f4fe3fbf8c0ebfd9f7e1a190471
Shawn M Moore <code@sartak.org>
2013-06-30 18:56:22 -0400

    Factor out an ItemMenu role

--- lib/TAEB/Announcement/Query/DropItems.pm
+++ lib/TAEB/Announcement/Query/DropItems.pm
@@ -2,26 +2,12 @@ package TAEB::Announcement::Query::DropItems;
 use Moose;
 use TAEB::OO;
 extends 'TAEB::Announcement::Query';
-with 'TAEB::Announcement::Role::SelectSubset';
+with (
+    'TAEB::Announcement::Role::SelectSubset',
+    'TAEB::Announcement::Role::ItemMenu',
+);
 
-sub BUILD {
-    my $self = shift;
-    my %inventory = map { $_->slot => $_ } TAEB->inventory_items;
-    my %menu_items = map { $_->selector => $_ } $self->all_menu_items;
-
-    for my $slot (keys %inventory) {
-        my $item = $inventory{$slot};
-
-        if (exists($menu_items{$slot})) {
-            $menu_items{$slot}->user_data($inventory{$slot});
-            delete $inventory{$slot};
-            delete $menu_items{$slot};
-        }
-    }
-
-    TAEB->log->inventory("Inventory out of sync: inventory item $_ is not in the menu") for values %inventory;
-    TAEB->log->inventory("Inventory out of sync: menu item $_ is not in inventory") for values %menu_items;
-}
+sub items_from { TAEB->inventory_items }
 
 __PACKAGE__->meta->make_immutable;
 
