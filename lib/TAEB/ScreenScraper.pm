3ad576a8a61015f536df99bdb24e0fd4f0ff5731
Sean Kelly <code@katron.org>
2013-06-30 22:04:10 -0500

    Add sell prompt

--- lib/TAEB/ScreenScraper.pm
+++ lib/TAEB/ScreenScraper.pm
@@ -868,6 +868,7 @@ our @prompts = (
     qr/^There is an? (.*) here, loot it\?/ => 'loot_it',
     qr/^Do you want to take something out of (?:the|.*'s) (.*)\?/ => 'take_something_out',
     qr/^Do you wish to put something in\?/ => 'put_something_in',
+    qr/^(.*?) offers (\d+) gold pieces? for (.*)\.  Sell it\?/ => 'sell_item',
 );
 
 our @exceptions = (
