ec80ea1dbe0b254953cfaa471b9e172e814e2efa
Shawn M Moore <code@sartak.org>
2013-07-04 11:42:57 -0400

    We now reconcile tile items in Scraper

--- lib/TAEB/Announcement/Query/PickupItems.pm
+++ lib/TAEB/Announcement/Query/PickupItems.pm
@@ -4,15 +4,6 @@ use TAEB::OO;
 extends 'TAEB::Announcement::Query';
 with 'TAEB::Announcement::Role::SelectSubset';
 
-sub BUILD {}
-after BUILD => sub {
-    my $self = shift;
-    my @expected = TAEB->current_tile->items;
-    my @menu_items = $self->all_menu_items;
-
-    # XXX reconcile @expected and @menu_items
-};
-
 __PACKAGE__->meta->make_immutable;
 
 1;
