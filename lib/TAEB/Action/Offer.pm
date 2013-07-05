070ca549876153f3ddc972e15c0b9c7d95916e6c
Shawn M Moore <code@sartak.org>
2013-07-05 13:06:44 -0400

    Better "did sacrifice" tracking

--- lib/TAEB/Action/Offer.pm
+++ lib/TAEB/Action/Offer.pm
@@ -10,13 +10,18 @@ has '+item' => (
     required => 1,
 );
 
+has did_sacrifice => (
+    is      => 'rw',
+    isa     => 'Bool',
+    default => 0,
+);
+
 sub respond_sacrifice_ground {
     my $self = shift;
     my $floor = shift;
     my $floor_item = TAEB->current_tile->find_item_maybe($floor);
 
     if ($self->item == $floor_item) {
-        $self->item->tried_to_sacrifice(1);
         return 'y';
     }
 
@@ -27,10 +32,8 @@ sub respond_sacrifice_what {
     my $self = shift;
 
     if (defined $self->item->slot) {
-        $self->item->tried_to_sacrifice(1);
         return $self->item->slot;
     }
-    TAEB->log->action("Unable to sacrifice '" . $self->item . "'. Sending escape, but I doubt this will work.", level => 'error');
 
     TAEB->send_message(check => 'inventory');
     TAEB->send_message(check => 'floor');
@@ -49,6 +52,16 @@ sub msg_sacrifice_gone {
         #because maybe_is used my remove_floor_item tries to match quantity
         TAEB->send_message(remove_floor_item => $item);
     }
+
+    $self->did_sacrifice(1);
+}
+
+sub done {
+    my $self = shift;
+
+    return if $self->did_sacrifice;
+
+    $self->item->failed_to_sacrifice(1);
 }
 
 __PACKAGE__->meta->make_immutable;
